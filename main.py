778aa1616c0e7f7c2f6addd8ffd4fc242ef3408c
archbober <git@archbober.com>
2025-12-01 16:12:02 +0100

    working connection vertex ai

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from google import genai
 from google.genai import types
+import sys
+import os
 
 from dotenv import load_dotenv
+
+print("got init")
 
 def main():
     load_dotenv()
@@ -17,5 +21,35 @@ def main():
         print("Audio-Text-Speach AI - no args")
         sys.exit(1)
 
-    api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
+    api_key = os.environ.get("YOUR_VERTEX_AI_API_KEY")
+    project_id = os.environ.get("PROJECT_ID")
+    project_location = os.environ.get("PROJECT_LOCATION")
+
+    print("got env")
+
+    client = genai.Client(
+        vertexai=True, api_key=api_key,
+    )
+
+    print("Client initialized. Sending request...")
+
+    try:
+        # Simple "Hello World" request
+        response = client.models.generate_content(
+            model="gemini-2.0-flash", 
+            contents="Are you online? Reply with 'Connection Successful'."
+        )
+        
+        print("\n--- Response ---")
+        print(response.text)
+        print("----------------")
+
+        if verbose:
+            print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+            print("Response tokens:", response.usage_metadata.candidates_token_count)
+        
+    except Exception as e:
+        print(f"\nError: {e}")
+
+if __name__ == "__main__":
+    main()
