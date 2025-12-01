e632748e8f0a1632552aea413aa36564f9caeba8
archbober <git@archbober.com>
2025-12-01 15:27:16 +0100

    config, args, api client

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+from google import genai
+from google.genai import types
+
+from dotenv import load_dotenv
+
 print("Hello world")
+
+def main():
+    load_dotenv()
+
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+
+    if not args:
+        # TODO implement args
+        print("Audio-Text-Speach AI - no args")
+        sys.exit(1)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
