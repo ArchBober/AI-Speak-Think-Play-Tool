d62b4e1440106ba9b3b3a5122f359893277a5ea5
archbober <git@archbober.com>
2025-12-01 18:59:39 +0100

    stt transcript to gemini

--- main.py
+++ main.py
@@ -24,54 +24,53 @@ def main():
         sys.exit(1)
 
     api_key = os.environ.get("YOUR_VERTEX_AI_API_KEY")
-    project_id = os.environ.get("PROJECT_ID")
-    project_location = os.environ.get("PROJECT_LOCATION")
 
-    print("got env")
+    # ctrl+c ctrl+v from wshiper readme but work perfectly
 
-    client = genai.Client(
-        vertexai=True, api_key=api_key,
-    )
-
-    print("Client initialized. Sending request...")
-
-    # try:
-    #     response = client.models.generate_content(
-    #         model="gemini-2.0-flash", 
-    #         contents="Are you online? Reply with 'Connection Successful'."
-    #     )
-        
-    #     print("\n--- Response ---")
-    #     print(response.text)
-    #     print("----------------")
-
-    #     if verbose:
-    #         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    #         print("Response tokens:", response.usage_metadata.candidates_token_count)
-        
-    # except Exception as e:
-    #     print(f"\nError: {e}")
-
-    model = whisper.load_model("turbo")
+    model_audio = whisper.load_model("turbo")
 
     # load audio and pad/trim it to fit 30 seconds
     audio = whisper.load_audio("Samples/message_accepted.wav")
     audio = whisper.pad_or_trim(audio)
 
     # make log-Mel spectrogram and move to the same device as the model
-    mel = whisper.log_mel_spectrogram(audio, n_mels=model.dims.n_mels).to(model.device)
+    mel = whisper.log_mel_spectrogram(audio, n_mels=model_audio.dims.n_mels).to(model_audio.device)
 
     # detect the spoken language
-    _, probs = model.detect_language(mel)
+    # TODO add loop if lang != EN or enforce EN transciption
+    _, probs = model_audio.detect_language(mel)
     print(f"Detected language: {max(probs, key=probs.get)}")
 
     # decode the audio
     options = whisper.DecodingOptions()
-    result = whisper.decode(model, mel, options)
+    result_audio_trans = whisper.decode(model_audio, mel, options)
 
     # print the recognized text
-    print(result.text)
+    print(result_audio_trans)
 
+
+    client = genai.Client(
+            vertexai=True, api_key=api_key,
+        )
+
+    print("Client initialized. Sending request...")
+
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.0-flash", 
+            contents=result_audio_trans.text
+        )
+        
+        print("\n--- Response ---")
+        print(response.text)
+        print("----------------")
+
+        if verbose:
+            print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+            print("Response tokens:", response.usage_metadata.candidates_token_count)
+        
+    except Exception as e:
+        print(f"\nError: {e}")
 
 
 
